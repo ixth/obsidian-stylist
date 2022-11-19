54b1011b3505efbf6e9aff50e1a7fdf2fbf54b53
Mikhail Menshikov <ainop@ya.ru>
2022-11-19 14:35:29 +0100

    Allow adding css classes to markdown blocks

--- main.ts
+++ main.ts
@@ -1,137 +1,48 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const CLASSNAME_TAG = 'classname:';
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+// not a real CSS parser, but works in most cases
+const makeScopedStyles = (rootSelector: string, source: string): string =>
+    source.trim().split(/(?<=\})/).map((ruleset) => {
+        const [selector, declaraions] = ruleset.split(/(?=\{)/);
+        return `:where(${rootSelector}) :where(${selector.trim().replace(/\n/, ' ')}) ${declaraions}`;
+    })
+    .join('');
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class extends Plugin {
+    private lastClassName: string | undefined;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    onload() {
+        this.registerMarkdownCodeBlockProcessor('style', (source, element) => {
+            const style = element.ownerDocument.createElement('style');
+            style.innerHTML = makeScopedStyles('.markdown-preview-view', source);
+            element.replaceWith(style);
+        });
 
-	async onload() {
-		await this.loadSettings();
+        this.registerMarkdownPostProcessor((element) => {
+            if (element.hasClass('next-class-skip')) {
+                return;
+            }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+            if (this.lastClassName !== undefined) {
+                element.classList.add(...this.lastClassName.split(/\s+/));
+                this.lastClassName = undefined;
+            }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+            const codeElements = element.querySelectorAll('code');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+            const classElement = [...codeElements].find(
+                (element) => element.innerText.trim().startsWith(CLASSNAME_TAG)
+            );
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+            if (classElement) {
+                this.lastClassName = classElement.innerText.slice(CLASSNAME_TAG.length);
+                if (classElement.parentElement?.childElementCount === 1) {
+                    classElement.parentElement.remove();
+                }
+                element.classList.add('next-class-skip');
+            }
+        });
+    }
+};
