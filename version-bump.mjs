c8a1455cda9ef1e7374c33bb77272d34d1b9f167
Mikhail Menshikov <ainop@ya.ru>
2022-11-26 18:07:03 +0100

    fix version-bump

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,16 @@
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from 'fs';
+
+const readJSON = (filename) =>
+    JSON.parse(readFileSync(filename, 'utf8'));
+
+const writeJSON = (filename, obj) => {
+    writeFileSync(filename, JSON.stringify(obj, null, '\t'));
+};
 
 const targetVersion = process.env.npm_package_version;
 
-// read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
-const { minAppVersion } = manifest;
-manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+const manifest = readJSON('manifest.json');
+writeJSON('manifest.json', { ...manifest, version: targetVersion });
 
-// update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+const versions = readJSON('versions.json');
+writeJSON('versions.json', { ...versions, [targetVersion]: manifest.minAppVersion });
